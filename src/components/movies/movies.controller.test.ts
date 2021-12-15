34dbd5b610640f8a2c097a30a515b370e152f9ac
entymon <pawel.olejniczak.i@gmail.com>
2021-12-15 10:51:11 +0000

    Write a unit tests

---
+++ src/components/movies/movies.controller.test.ts
@@ -0,0 +1,14 @@
+import MoviesController from "./movies.controller";
+
+describe('MoviesController', () => {
+
+  describe('createMovie', () => {
+    it ('returns success response for no errors', () => {
+      expect(2).toBe(2)
+    })
+  
+    it ('', () => {
+      expect(2).toBe(2)
+    })
+  })
+})
